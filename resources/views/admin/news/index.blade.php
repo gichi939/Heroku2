a098edd9d2c5d2a21e4858f97441a98fdbf71102
EC2 Default User <ec2-user@ip-172-31-28-13.us-east-2.compute.internal>
2020-06-23 05:08:24 +0000

    commit

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <h2>ニュース一覧</h2>
-        </div>
+        </div> 
         <div class="row">
             <div class="col-md-4">
                 <a href="{{ action('Admin\NewsController@add') }}" role="button" class="btn btn-primary">新規作成</a>
