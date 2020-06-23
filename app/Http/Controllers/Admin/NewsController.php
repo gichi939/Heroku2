8a2c8691c41704230ad646aeb3d3b5d9356762f9
EC2 Default User <ec2-user@ip-172-31-28-13.us-east-2.compute.internal>
2020-06-23 04:59:26 +0000

    commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -81,8 +81,8 @@ class NewsController extends Controller
        if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
